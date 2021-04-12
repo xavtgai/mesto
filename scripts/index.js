2e87098031c107c304a926830f8c93c098a16e20
ilyagr <altaica@narod.ru>
2021-04-12 09:16:12 +0300

    final ver 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('job');
+
 
 let profileTitle = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
@@ -20,11 +22,9 @@ function closePopup() {
 
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let popup__overlay = document.querySelector('.popup__overlay');
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-//popup__overlay.addEventListener('click', function() { closePopup(); })
 
 
 function formSubmitHandler(evt) {
